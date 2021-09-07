18d55d38fce0d3a37f071a9f0b037a9fa0c135d0
someGenki <1159140147@qq.com>
2021-09-07 18:00:22 +0800

    refactor: 更新登录页面

---
+++ src/utils/refreshToken.ts
@@ -0,0 +1,34 @@
+class RefreshToken{
+  private refreshing: boolean;
+  private waitingQueue: any[];
+
+  constructor() {
+    this.refreshing = false;
+    this.waitingQueue = [];
+  }
+
+  // 用于排除不需要token的接口，避免死循环请求
+  private isNeedTokenURL(url, arr = ['/login', '/refresh', '/getUserInfo']) {
+    return !arr.some((val) => url.indexOf(val) > -1);
+  }
+  // 返回一个promise对象，用于阻塞调用者继续执行。这里面发送刷新请求，成功后清空请求队列
+  private refreshTokenBeforeReq(doRefreshTokenApi: () => Promise<any>): Promise<unknown> {
+    // 创建一个未完成的promise，把改变状态的resolve方法交给请求token结束后执行
+    const promise = new Promise((resolve) => {
+      console.log('等待新token');
+      // 等待队列放的是一个回调函数，来延迟resolve的执行，以此控制promise状态的改变
+      this.waitingQueue.push(() => resolve(null));
+    });
+    if (!this.refreshing) {
+      this.refreshing = true;
+      // 模拟请求刷新Token接口，当接口返回数据时执行then方法 TODO 添加catch捕获异常
+      doRefreshTokenApi().then(() => {
+        console.log('刷新token成功，放行队列中的请求');
+        this.refreshing = false;
+        this.waitingQueue.forEach((cb) => cb());
+        this.waitingQueue.length = 0;
+      });
+    }
+    return promise;
+  }
+}
--- src/views/sys/login/index.vue
+++ src/views/sys/login/index.vue
@@ -1,28 +1,106 @@
 <template>
-  <div class="login-container">
-    <span style="cursor: pointer" @click="changeLoginStyle">toggle</span>
-    <login01 v-if="pageNo === '1'" />
-    <login02 v-else-if="pageNo === '2'" />
+  <div class="admin-login">
+    <div class="login-container">
+      <div class="login-left">
+        <h1>准备摆个LOGO</h1>
+        <h2>其次是个图片，跟vben类似</h2>
+      </div>
+      <div class="login-right">
+        <el-form
+          :model="form"
+          :rules="loginRules"
+          ref="loginFormRef"
+          class="login-form"
+        >
+          <h1 style="margin-left: 4px; text-align: left">登录</h1>
+          <p>admin admin admin</p>
+          <el-form-item required size="small" prop="username">
+            <input
+              class="login-input"
+              required
+              v-model="form.username"
+              placeholder="用户名/邮箱"
+            />
+          </el-form-item>
+
+          <el-form-item required prop="password">
+            <input
+              class="login-input"
+              :type="showPassword ? '' : 'password'"
+              required
+              v-model="form.password"
+              placeholder="请输入密码"
+            />
+            <i
+              @click="showPasswordToggle"
+              :class="{ slash: showPassword }"
+              class="el-icon-view password-see"
+            />
+          </el-form-item>
+
+          <div
+            style="
+              display: flex;
+              justify-content: space-between;
+              margin-bottom: 22px;
+            "
+          >
+            <input
+              placeholder="验证码"
+              class="login-input"
+              style="width: 50%; height: 48px"
+              type="text"
+              value="aaaa"
+            />
+            <img
+              style="width: 120px; height: 44px"
+              src="http://www.webxml.com.cn/WebServices/ValidateCodeWebService.asmx/cnValidateImage?byString=aaaa"
+              alt="验证码"
+            />
+          </div>
+          <el-button
+            style="width: 100%"
+            type="primary"
+            @click.prevent="handleLogin"
+            >登录
+          </el-button>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Login01 from './Login01.vue'
-import Login02 from './Login02.vue'
+// import Login01 from './Login01.vue'
+// import Login02 from './Login02.vue'
 import { useRoute } from 'vue-router'
 import { ref } from 'vue'
+import useLogin from './useLogin'
 // https://element-plus.gitee.io/#/zh-CN/component/form
 export default {
   name: 'login',
-  components: { Login01, Login02 },
+  // components: { Login01, Login02 },
   setup() {
     // 测试用，根据page参数来显示对应的登录页1
     const pageNo = ref(useRoute().query.page || '1')
+
     const changeLoginStyle = () => {
       if (pageNo.value === '1') pageNo.value = '2'
       else pageNo.value = '1'
     }
-    return { pageNo, changeLoginStyle }
+    const showPassword = ref(false)
+
+    function showPasswordToggle() {
+      showPassword.value = !showPassword.value
+    }
+
+    return {
+      pageNo,
+      showPasswordToggle,
+      showPassword,
+      changeLoginStyle,
+      ...useLogin(),
+    }
   },
 }
 </script>
@@ -31,29 +109,109 @@ export default {
 $bg-input: #f1f2f3;
 $bg-button: #cfd3d7;
 
-.login-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  min-height: 100vh;
-  padding-top: 10px;
+.admin-login {
+  position: relative;
+  height: 100vh;
+  padding: 0 1rem;
   overflow: hidden;
-  background: linear-gradient(226deg, #e0eafc, #c6d5ec);
+  background-image: linear-gradient(to right, #4facfe 0%, #00f2fe 100%);
 }
 
-.login-button {
-  width: 100%;
-  margin-top: 10px;
-}
-
-.login-help {
+.login-container {
+  position: relative;
   display: flex;
-  justify-content: space-between;
+  align-items: center;
+  justify-content: space-around;
+  height: 100%;
+  // TODO 响应式
 }
 
-.forget-password {
-  font-size: 0.9rem;
-  color: var(--primary-text-color, $primary-text-color);
+.login-left {
+  width: 25%;
+  min-width: 300px;
+  max-width: 500px;
+  height: 500px;
+  background-size: 100%;
+  //background-image: url('https://mixkit.imgix.net/art/preview/mixkit-i-love-you-hand-gesture-419-original-large.png?q=80&auto=format%2Ccompress');
+}
+
+.login-right {
+  display: flex;
+  justify-content: flex-end;
+  width: 50%;
+  height: 500px;
+}
+
+.login-form {
+  align-self: center;
+  //height: 450px;
+  width: 450px;
+  padding: 0 2rem 3rem;
+  background: white;
+  border-radius: 1rem;
+  box-shadow: 4px 10px 16px rgb(36 37 38 / 13%);
+}
+
+// 密码可见图标 el-view
+.password-see {
+  position: absolute;
+  top: 12px;
+  right: 14px;
+  font-size: 1.4rem;
+  color: #666;
+  cursor: pointer;
+}
+
+// 给图标画一条斜线
+.password-see.slash::after {
+  position: absolute;
+  right: 10px;
+  width: 2px;
+  height: 22px;
+  content: '';
+  background-color: #666;
+  transform: rotate(45deg);
+}
+
+// 原生输入框样式
+.login-input {
+  position: relative;
+  width: 100%;
+  height: 44px;
+  padding: 12px;
+  font-size: 1.2rem;
+  //text-align: center; // 让光标和内容居中
+  background-color: $bg-input;
+  border: none;
+  border-radius: 4px;
+  outline: none;
+  caret-color: var(--primary-color, #02bf6f);
+
+  &::placeholder {
+    //color: #a1a1a1;
+    text-align: center;
+    transition: 0.25s;
+  }
+
+  &:focus::placeholder {
+    opacity: 0;
+  }
+}
+
+@media screen and (max-width: $sm-width) {
+  .login-left {
+    display: none;
+  }
+
+  .login-right {
+    align-self: flex-start;
+    justify-content: center;
+    width: 100%;
+  }
+
+  .login-form {
+    padding-right: 1rem;
+    padding-left: 1rem;
+  }
 }
 </style>

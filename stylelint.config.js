a86246170afa33e45a19a024ed18a09ddf8cff06
hejiyuan <1159140147@qq.com>
2021-11-24 15:44:18 +0800

    chore: stylelint update to V14,and update the stylelint.config.js

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,54 +2,19 @@
 // vue项目中使用新的单文件组件样式的特性（状态驱动的动态 CSS | v-bind(var) )时，值需要用引号包裹起来，避免被stylelint所格式化
 module.exports = {
   root: true,
-  plugins: ['stylelint-order', 'stylelint-scss'],
   extends: [
-    // > 该风格是 Stylelint 的维护者汲取了 GitHub、Google、Airbnb 多家之长生成的一套css风格规则
-    'stylelint-config-standard',
-    // 兼容于prettier的规则，避免冲突
+    'stylelint-config-recommended-scss',
+    'stylelint-config-recommended-vue',
     'stylelint-config-prettier',
-    // 对css进行排序 https://github.com/stormwarning/stylelint-config-recess-order/blob/main/index.js
+    // 对css进行排序 → https://github.com/stormwarning/stylelint-config-recess-order/blob/main/index.js
     'stylelint-config-recess-order',
   ],
   rules: {
-    'selector-pseudo-class-no-unknown': null,
-    'selector-pseudo-element-no-unknown': null,
-    'at-rule-no-unknown': [
-      true,
-      {
-        ignoreAtRules: [
-          'tailwind',
-          'return',
-          'at-root',
-          'apply',
-          'variants',
-          'responsive',
-          'screen',
-          'function',
-          'if',
-          'else',
-          'each',
-          'include',
-          'mixin',
-        ],
-      },
-    ],
+    'at-rule-no-unknown': null,
     'no-empty-source': null,
-    'named-grid-areas-no-invalid': null,
-    'unicode-bom': 'never',
     'no-descending-specificity': null,
-    'media-feature-name-no-vendor-prefix': true,
-    'font-family-no-missing-generic-family-keyword': null,
-    'declaration-colon-space-after': 'always-single-line',
-    'declaration-colon-space-before': 'never',
-    // 'declaration-block-trailing-semicolon': 'always',
-    'rule-empty-line-before': [
-      'always',
-      {
-        ignore: ['after-comment', 'first-nested'],
-      },
-    ],
-    'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }],
+    'scss/at-import-partial-extension': null,
+    'scss/at-import-no-partial-leading-underscore': null,
   },
   ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts'],
 }

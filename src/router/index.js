6d6358b37c6b87f9e1cdf5cb95809052bb4fd128
hejiyuan <1159140147@qq.com>
2022-03-25 22:23:33 +0800

    perf: 优化路由相关配置信息

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,27 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import {
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+} from 'vue-router'
 import { getToken } from '/src/utils/storage'
 import { constRoutes } from './modules/const'
 import { basicRoutes } from './modules/basic'
 import { useUserStore } from '/src/store/user'
 import { useLayoutStore } from '/src/store/layout'
 
-// [vue-router官方文档指路]:(https://next.router.vuejs.org/zh/guide/index.html)
+const base = import.meta.env.BASE_URL
+const mode = import.meta.env.VITE_ROUTER_HISTORY
+const historyMode = // 从.env文件中读取配置判断是否hash模式还是history模式
+  mode === 'hash' ? createWebHashHistory(base) : createWebHistory(base)
+const pageTitle = import.meta.env.VITE_DEFAULT_TITLE
 
 // 定义一个公共路径集合，任何用户及匿名者都能访问的到
 export const PUBLIC_PATH = new Set()
-
 basicRoutes.forEach((item) => PUBLIC_PATH.add(item.path))
 
+// [vue-router官方文档指路]:(https://next.router.vuejs.org/zh/guide/index.html)
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: historyMode,
   routes: constRoutes,
   strict: true, // 禁止尾随斜杠
 })
@@ -30,15 +38,15 @@ const router = createRouter({
  */
 router.beforeEach(async (to) => {
   // 根据是否有 token 判断用户是否登录
-  let token = getToken()
+  const token = getToken()
   // 如果[未登录]且要访问[不在]公共路径集合里的路径时，跳转到登录页面并记录之前的页面用于重新访问
   if (!token && !PUBLIC_PATH.has(to.path))
     return { path: '/login', query: { redirect: to.fullPath } }
   const userStore = useUserStore()
   // 如果已登录但因为刷新后导致保存在内存中的数据(登录信息，动态添加的路由等)丢失，
   // 需要再次发起请求重新获取用户信息，并动态添加路由
-  if (userStore.hasUserInfo === false) {
-    await userStore.getUserInfo()
+  if (!userStore['hasUserInfo']) {
+    await userStore['hasUserInfo']()
     // 要添加个catch处理错误
     return to
   }
@@ -48,9 +56,9 @@ router.beforeEach(async (to) => {
  * 全局后置钩子 ：它们对于分析、更改页面标题、声明页面等辅助功能以及许多其他事情都很有用。
  */
 router.afterEach((to) => {
-  document.title = to.meta.title || import.meta.env.VITE_DEFAULT_TITLE
+  document.title = to.meta.title || pageTitle
   // 记录访问过的页面
-  useLayoutStore().accessRecord(to)
+  useLayoutStore()['accessRecord'](to)
 })
 
 export default router
--- src/utils/convert.js
+++ src/utils/convert.js
@@ -1,10 +1,17 @@
-const camelizeRE = /-(\w)/g
-
 /**
  * 将中划线格式字符串驼峰化
- * @example jo-jo => joJo
+ * @example first-name => firstName
  * @from vue-next/packages/shared/src/index.ts
  */
 export function camelize(str) {
-  return str.replace(camelizeRE, (_, c) => (c ? c.toUpperCase() : ''))
+  return str.replace(/-(\w)/g, (_, c) => (c ? c.toUpperCase() : ''))
+}
+
+/**
+ * 将驼峰化(Lower Camel Case)转换成Kebab Case
+ * 大驼峰的化要自己处理下前面的 - 号
+ * @example firstName => first-name
+ */
+export function KebabCase(str) {
+  return str.replace(/[A-Z]/g, (_, c) => '-' + c.toLowerCase())
 }
